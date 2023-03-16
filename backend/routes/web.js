03feab4999f29551fcf0b0dfd69b040717e7c3fb
dernoczidaniel <dernoczi.daniel@students.jedlik.eu>
2023-03-16 13:17:00 +0100

    delete backend

--- backend/routes/web.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import StudentController from '../controllers/studentController.js';
-const router = express.Router();
-
-router.get('/student', StudentController.getAllDoc)
-
-export default router
