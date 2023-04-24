2b64d2e729e9a93159ac058fa1d3a0d714310905
Hujber Patrik Tamás <hujber.patrik.tamas@students.jedlik.eu>
2023-04-24 11:56:38 +0200

    Profile

--- vuejsfrontend/src/App.vue
+++ vuejsfrontend/src/App.vue
@@ -68,65 +68,79 @@ export default {
         <div class="container-fluid bg-dark px-0 menu" id="oldalteto">
             <div class="my-element animate__animated animate__fadeIn" :style="{ 'animation-delay': '0.3s' }">
 
-            <div class="row gx-0">
-                <div class="col-lg-3 bg-dark d-none d-lg-block">
-                    <a href="/" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
-                        <img src="../src/assets/img/Logo/Ford_Motor_Company_Logo.svg.png" alt="Logo" width="120"
-                            height="60">
-                    </a>
-                </div>
-                <div class="col-lg-9">
-                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0 px-lg-5">
-                        <a href="/" class="navbar-brand d-block d-lg-none">
-                            <h1 class="m-0 display-4 text-primary text-uppercase">Ford</h1>
+                <div class="row gx-0">
+                    <div class="col-lg-3 bg-dark d-none d-lg-block">
+                        <a href="/"
+                            class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                            <img src="../src/assets/img/Logo/Ford_Motor_Company_Logo.svg.png" alt="Logo" width="120"
+                                height="60">
                         </a>
+                    </div>
+                    <div class="col-lg-9">
+                        <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0 px-lg-5">
+                            <a href="/" class="navbar-brand d-block d-lg-none">
+                                <h1 class="m-0 display-4 text-primary text-uppercase">Ford</h1>
+                            </a>
 
-                        <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
-                            data-bs-target="#navbarCollapse">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                            <div class="navbar-nav mr-auto py-0">
-                                <router-link class="nav-link" to="/">Kezdőlap</router-link>
-                                <div class="nav-item dropdown">
-                                    <a href="#" class="nav-link dropdown-toggle"
-                                        data-bs-toggle="dropdown">Modellválaszték</a>
-                                    <div class="dropdown-menu rounded-0 m-0 align-items-center">
-                                        <div v-for="car in models" :value="car">
-                                            <img :src="car.img_url" :alt="car.img_url" width="190" height="120">
-                                            <router-link to="/Cars"><button class="btn  py-1 px-0">{{ car.name
-                                            }}</button></router-link>
+                            <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+                                data-bs-target="#navbarCollapse">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                                <div class="navbar-nav mr-auto py-0">
+                                    <router-link class="nav-link" to="/">Kezdőlap</router-link>
+                                    <div class="nav-item dropdown">
+                                        <a href="#" class="nav-link dropdown-toggle"
+                                            data-bs-toggle="dropdown">Modellválaszték</a>
+                                        <div class="dropdown-menu rounded-0 m-0 align-items-center">
+                                            <div v-for="car in models" :value="car">
+                                                <img :src="car.img_url" :alt="car.img_url" width="190" height="120">
+                                                <router-link to="/Cars"><button class="btn  py-1 px-0">{{ car.name
+                                                }}</button></router-link>
+                                            </div>
+                                            <div style="background-color: #1351d8;">
+                                                <router-link class="nav-link" to="/Cars">Minden modell</router-link>
+                                            </div>
                                         </div>
-                                        <div style="background-color: #1351d8;">
-                                            <router-link class="nav-link" to="/Cars">Minden modell</router-link>
+                                    </div>
+                                    <router-link class="nav-link" to="/Service">Szervíz</router-link>
+                                    <a href="#kapcsolat" class="nav-link">Kapcsolat</a>
+                                    <div class="nav-item dropdown">
+                                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Továbbiak</a>
+                                        <div class="dropdown-menu rounded-0 m-0">
+                                            <router-link class="dropdown-item" to="/History">Története</router-link>
+                                            <router-link class="dropdown-item" to="/About">Rólunk</router-link>
+                                            <router-link class="dropdown-item" to="/News">Hírek</router-link>
+                                            <router-link class="dropdown-item" to="/Boiling">Források</router-link>
+                                            <router-link class="dropdown-item" to="/login"  v-if="isLoggedIn == false">Bejelentkezés</router-link>
+
+                                            <router-link class="dropdown-item" to="/Profile "  v-if="isLoggedIn "> <i class="fas fa-user"></i>
+                                                Profil</router-link>
+
                                         </div>
                                     </div>
                                 </div>
-                                <router-link class="nav-link" to="/Profile">Ford fiók</router-link>
-                                <router-link class="nav-link" to="/Service">Szervíz</router-link>
-                                <a href="#kapcsolat" class="nav-link">Kapcsolat</a>
-                                <div class="nav-item dropdown">
-                                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Továbbiak</a>
-                                    <div class="dropdown-menu rounded-0 m-0">
-                                        <router-link class="dropdown-item" to="/History">Története</router-link>
-                                        <router-link class="dropdown-item" to="/About">Rólunk</router-link>
-                                        <router-link class="dropdown-item" to="/News">Hírek</router-link>
-                                        <router-link class="dropdown-item" to="/Boiling">Források</router-link>
-                                        <router-link class="dropdown-item" to="/login">bejelentkezés</router-link>
-                                    </div>
-                                </div>
+                                <router-link to="/login" class="btn btn-primary py-md-3 px-md-5 d-none d-lg-block"
+                                    v-if="isLoggedIn == false">Bejelenkezés</router-link>
+
+                                <button
+                                    class=" col-lg-2 col-md-2 col-2 btn btn-primary py-md-3 px-md-5 d-none d-lg-block m-1"
+                                    href="/Profile" v-if="isLoggedIn">
+                                    <i class="fas fa-user"></i>
+                                    Profil
+                                </button>
+
+                                <button class="btn btn-danger py-md-3 px-md-5 d-none d-lg-block m-1" @click="logout"
+                                    v-if="isLoggedIn">
+                                    Kijelentkezés
+                                </button>
+
+
+
                             </div>
-                            <router-link to="/login" class="btn btn-primary py-md-3 px-md-5 d-none d-lg-block"
-                                v-if="isLoggedIn == false">Bejelenkezés</router-link>
-
-
-                            <button class="btn btn-danger py-md-3 px-md-5 d-none d-lg-block" @click="logout"
-                                v-if="isLoggedIn">Logout</button>
-
-                        </div>
-                    </nav>
+                        </nav>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
 
